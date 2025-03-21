1da91c767384d2ade34bc724a8c695c120fda2dc
pathakkunal <139622922+pathakkunal@users.noreply.github.com>
2025-03-21 05:51:25 +0530

    Add files via upload

---
+++ 1.1 Aadding_two_Numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+     
+     int n,m;
+    printf("Enter first Numbers:\n");
+    scanf("%d",&n);
+    printf("Enter Second Numbers:\n");
+    scanf("%d",&m);
+    int total=n+m;
+    printf("Total:");
+    printf("%d",total);
+
+    return 0;
+}
